420b761bb61d6e546b34b17858c8a6bb38b0fa31
kCMI113 <cmkang1316@gmail.com>
2023-07-13 22:34:42 +0900

    IMR-75 feat: add num img in file name

--- img_main.py
+++ img_main.py
@@ -23,7 +23,7 @@ def main(config: DictConfig = None) -> None:
     log.info("Num of failed crawling: %d", failed_crawling)
 
     # save crwaling results
-    csv_path = os.path.join(config.out_dir, f"{config.origin_Tag}_{config.query_Tag}_{config.keys_filename}")
+    csv_path = os.path.join(config.out_dir, f"{len(Img_infos)}_{config.origin_Tag}_{config.query_Tag}_{config.keys_filename}")
     log.info("Save concatencated Img_infos to %s", csv_path)
     Img_infos_df = pd.DataFrame(Img_infos)
     Img_infos_df.to_csv(csv_path, index=False)
